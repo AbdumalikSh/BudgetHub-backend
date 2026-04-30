047e56f404d32ac641dcd5c90b6b28625834c4f2
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-04-30 23:29:47 +0200

    Dashboard

---
+++ server/routes/dashboardRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { protect } = require("../middlewares/AuthMiddleware.js");
+const { getDashboardData } = require("../controllers/dashboardController.js");
+
+const router = express.Router();
+
+router.get("/", protect, getDashboardData);
+
+module.exports = router;
