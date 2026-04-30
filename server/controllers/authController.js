0f5467c8129d011e3035d4e5c72e9df894ffe3ab
Abdumalik Shukrilloev <shamf120804@gmail.com>
2026-04-30 16:06:48 +0200

    Get User Info

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -54,11 +54,21 @@ exports.loginUser = async (req, res) => {
       token: generateToken(user._id),
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error registering user", error: error.message });
+    res.status(500).json({ message: "Error login user", error: error.message });
   }
 };
 
 //User Info
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error Getting User Info", error: error.message });
+  }
+};
